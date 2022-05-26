2dcaa345cd213aafe272823043b2502fffb7c34a
Teresa <teriv1707@gmail.com>
2022-05-26 16:28:28 +0200

    modified

--- app.js
+++ app.js
@@ -1,64 +1,10 @@
-// class Weather {
-//   constructor(date, time, humidity, pressure, temp, description, speed,src, deg /*, parent*/ ){
-//     this.date = date;
-//     this.time = time;
-//     this.humidity = humidity;
-//     this.pressure = pressure;
-//     this.temp = temp;
-//     this.description = description;
-//     this.speed = speed;
-//     this.src = src;
-//     this.deg = deg; 
-//     // this.parent = document.querySelector(parent);
-//   }
-//   render(){
-//     let weather = document.querySelector(".weather");
-//     let icon = document.querySelector(".icon");
-//     let time = document.querySelector(".time");
-//     let src = document.getElementsByTagName("src")
-
-//     console.log(weather, icon, time, src)
-
-//     weather.innerHTML = 
-
-//   `<div class="weather">
-//     <div class="date">${this.date}</div>
-//     <div class="time">${this.time}</div>
-//     <div class="humidity">${this.humidity}</div>
-//     <div class="pressure">${this.pressure}</div>
-//     <div class="temp">${this.temp}</div>
-//     <div class="description">${this.description}</div>
-//     <div class="speed">${this.speed}</div>
-//     <div class="deg">${this.deg}</div>
-//     <img class="icon" src="${this.src}">
-    
-//   </div>`;
-
-//     // this.parent.append(div);
-
-//   }
-// }
-
-// new Weather(
-// this.date = "15.25.2022",
-// this.time = "13.04",
-// this.humidity = "weather.",
-// this.pressure = "pressure;",
-// this.temp = "temp;",
-// this.description = "description;",
-// this.speed = "speed;",
-// this.src = "some",
-// this.deg = "deg;",
-// // this.div = ".weather"
-// ).render();
-
 
 
 function getValue() {
   
  let input = document.getElementById("loc").value;
   
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&APPID=5d066958a60d315387d9492393935c19 `)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&APPID=5d066958a60d315387d9492393935c19 `)
   .then((res) => res.json())
   .then((weather) => {console.log(weather);
       
